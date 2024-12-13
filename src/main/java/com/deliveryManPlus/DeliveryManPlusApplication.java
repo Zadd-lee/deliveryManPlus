1092c56799d662ec40dfba35394feb61dbf0393b
이자드 <kimminkyeong0418@gmail.com>
2024-12-13 11:46:46 +0900

    build: 기본 프로젝트 세팅

---
+++ src/main/java/com/deliveryManPlus/DeliveryManPlusApplication.java
@@ -0,0 +1,13 @@
+package com.deliveryManPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryManPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryManPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deliveryManPlus/DeliveryManPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveryManPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryManPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

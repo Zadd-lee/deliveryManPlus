ab06c88abf09829d24d3c194e98300e18960e117
이자드 <kimminkyeong0418@gmail.com>
2024-12-19 17:34:43 +0900

    feat : 전체 조회 출력 내용 수정

--- src/main/java/com/deliveryManPlus/shop/model/dto/ShopResponseDto.java
+++ src/main/java/com/deliveryManPlus/shop/model/dto/ShopResponseDto.java
@@ -5,29 +5,19 @@ import com.deliveryManPlus.shop.model.entity.Shop;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
-
 @NoArgsConstructor
 @Getter
 public class ShopResponseDto {
     private Long id;
     private String name;
     private String address;
-    private BigDecimal minimumOrderAmount;
     private ShopStatus status;
-    private String openAt;
-    private String closedAt;
-    private String closedDay;
 
 
     public ShopResponseDto(Shop shop) {
         this.id = shop.getId();
         this.name = shop.getName();
         this.address = shop.getAddress();
-        this.minimumOrderAmount = shop.getMinimumOrderAmount();
         this.status = shop.getStatus();
-        this.openAt = shop.getOpenAt();
-        this.closedAt= shop.getClosedAt();
-        this.closedDay = shop.getClosedDay();
     }
 }

f4e3db2f7b68a8ac04883f7bab6df4eea5b7a5da
이자드 <kimminkyeong0418@gmail.com>
2024-12-24 23:35:29 +0900

    refactor: entity 검증 로직 분리

---
+++ src/main/java/com/deliveryManPlus/common/utils/EntityValidator.java
@@ -0,0 +1,27 @@
+package com.deliveryManPlus.common.utils;
+
+import com.deliveryManPlus.menu.model.entity.Menu;
+import com.deliveryManPlus.shop.model.entity.Shop;
+
+public class EntityValidator {
+
+    /**
+     * 사용자가 가게의 소유자인지 확인
+     * @param userId
+     * @param shop
+     * @return
+     */
+    public static boolean isValid(Long userId, Shop shop) {
+        return shop.getOwner().getId().equals(userId);
+    }
+
+    /**
+     * 메뉴가 가게의 메뉴인지 확인
+     * @param shopId
+     * @param menu
+     * @return
+     */
+    public static boolean isValid(Long shopId, Menu menu) {
+        return menu.getShop().getId().equals(shopId);
+    }
+}

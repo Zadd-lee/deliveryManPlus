669340d44072c19be3823a29f5ed82fa7ac8c7f1
이자드 <kimminkyeong0418@gmail.com>
2025-01-12 13:51:25 +0900

    feat : logout 로직 변경

--- src/main/java/com/deliveryManPlus/auth/controller/AuthController.java
+++ src/main/java/com/deliveryManPlus/auth/controller/AuthController.java
@@ -5,10 +5,14 @@ import com.deliveryManPlus.auth.model.dto.LoginRequestDto;
 import com.deliveryManPlus.auth.model.dto.SigninRequestDto;
 import com.deliveryManPlus.auth.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,16 +33,22 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<JwtAuthResponseDto> login(@Valid @RequestBody LoginRequestDto dto, HttpServletRequest request) {
+    public ResponseEntity<JwtAuthResponseDto> login(@Valid @RequestBody LoginRequestDto dto) {
         return new ResponseEntity<>(authService.login(dto),HttpStatus.OK);
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<Void> logout(HttpServletRequest request) {
-        //만료
-        request.getSession().invalidate();
+    public ResponseEntity<Void> logout(HttpServletRequest request,
+                                       HttpServletResponse response, Authentication authentication)
+            throws UsernameNotFoundException {
+        // 인증 정보가 있다면 로그아웃 처리.
+        if (authentication != null && authentication.isAuthenticated()) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        // 인증 정보가 없다면 인증되지 않았기 때문에 로그인 필요.
+        throw new UsernameNotFoundException("로그인이 먼저 필요합니다.");
     }
-
 }

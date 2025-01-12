92ab376ceff1b1c03d859578d52a6d956bcf0376
이자드 <kimminkyeong0418@gmail.com>
2025-01-12 14:15:55 +0900

    feat : menu 로직 변경

--- src/main/java/com/deliveryManPlus/menu/service/imp/MenuServiceImp.java
+++ src/main/java/com/deliveryManPlus/menu/service/imp/MenuServiceImp.java
@@ -18,8 +18,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static com.deliveryManPlus.common.utils.EntityValidator.isValid;
-
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -29,13 +27,12 @@ public class MenuServiceImp implements MenuService {
     private final ShopRepository shopRepository;
 
     @Override
-    public void create(Long userId, Long shopId, MenuCreateRequestDto dto) {
+    public void create(Long shopId, MenuCreateRequestDto dto) {
         //검증
         //todo 폐업 제외 검색 리팩토링 진행할 것.
         Shop shop = shopRepository.findById(shopId)
                 .orElseThrow(() -> new ApiException(ShopErrorCode.NOT_FOUND));
 
-        validateShopAndUser(shop, userId);
 
         Menu menu = dto.toEntity();
         menu.updateShop(shop);
@@ -45,60 +42,39 @@ public class MenuServiceImp implements MenuService {
     }
 
     @Override
-    public void update(Long userId, Long shopId, Long menuId, @Valid MenuUpdateRequestDto dto) {
+    public void update(Long shopId, Long menuId, @Valid MenuUpdateRequestDto dto) {
         //검증
-        Shop shop = shopRepository.findById(shopId)
+        Shop shop = shopRepository.findById(menuId)
                 .orElseThrow(() -> new ApiException(ShopErrorCode.NOT_FOUND));
 
         Menu menu = menuRepository.findById(menuId)
                 .orElseThrow(() -> new ApiException(MenuErrorCode.NOT_FOUND));
-
-        validateShopAndMenu(menu, shop);
-        validateShopAndUser(shop, userId);
 
         menu.updateByDto(dto);
 
     }
 
     @Override
-    public void updateStatus(Long userId, Long shopId, Long menuId, MenuUpdateStatusRequestDto dto) {
+    public void updateStatus(Long shopId, Long menuId, MenuUpdateStatusRequestDto dto) {
         //검증
         Shop shop = shopRepository.findById(shopId)
                 .orElseThrow(() -> new ApiException(ShopErrorCode.NOT_FOUND));
 
         Menu menu = menuRepository.findById(menuId)
                 .orElseThrow(() -> new ApiException(MenuErrorCode.NOT_FOUND));
-
-        validateShopAndMenu(menu, shop);
-        validateShopAndUser(shop, userId);
 
         menu.updateStatus(dto.getStatus());
     }
 
     @Override
-    public void delete(Long userId, Long shopId, Long menuId) {
+    public void delete(Long shopId, Long menuId) {
         //검증
         Shop shop = shopRepository.findById(shopId)
                 .orElseThrow(() -> new ApiException(ShopErrorCode.NOT_FOUND));
 
         Menu menu = menuRepository.findById(menuId)
                 .orElseThrow(() -> new ApiException(MenuErrorCode.NOT_FOUND));
-
-        validateShopAndMenu(menu, shop);
-        validateShopAndUser(shop, userId);
-
         menu.updateStatus(MenuStatus.NOT_USE);
     }
 
-    private static void validateShopAndMenu(Menu menu, Shop shop) {
-        if (isValid(shop.getId(), menu)) {
-            throw new ApiException(MenuErrorCode.NOT_FOUND);
-        }
-    }
-
-    private static void validateShopAndUser(Shop shop, Long userId) {
-        if (isValid(userId, shop)) {
-            throw new ApiException(MenuErrorCode.UNAUTHORIZED);
-        }
-    }
 }

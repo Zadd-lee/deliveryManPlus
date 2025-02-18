d739f77e3afe5d5966a65fdf9439a683edc79e07
이자드 <kimminkyeong0418@gmail.com>
2025-02-18 17:33:02 +0900

    feat : 상점 - 사장님 swagger 적용 완료

--- src/main/java/com/deliveryManPlus/dto/shop/ShopStatusRequestDto.java
+++ src/main/java/com/deliveryManPlus/dto/shop/ShopStatusRequestDto.java
@@ -1,13 +1,16 @@
 package com.deliveryManPlus.dto.shop;
 
 import com.deliveryManPlus.constant.ShopStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "상점 상태 수정 요청 DTO")
 @Getter
 @NoArgsConstructor
 public class ShopStatusRequestDto {
+    @Schema(description = "상점 상태", example = "OPEN", allowableValues = {"OPEN", "CLOSED"})
     @NotNull
     private ShopStatus status;
 }

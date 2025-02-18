bcf78b5ce054d8d1c195902847672ef93a84b980
이자드 <kimminkyeong0418@gmail.com>
2025-02-18 16:56:16 +0900

    feat : auth swagger 적용 완료

--- src/main/java/com/deliveryManPlus/controller/AuthController.java
+++ src/main/java/com/deliveryManPlus/controller/AuthController.java
@@ -4,6 +4,9 @@ import com.deliveryManPlus.dto.auth.JwtAuthResponseDto;
 import com.deliveryManPlus.dto.auth.LoginRequestDto;
 import com.deliveryManPlus.dto.auth.SigninRequestDto;
 import com.deliveryManPlus.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -21,22 +24,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
+@Tag(name = "인증/인가", description = "로그인, 로그아웃, 회원가입 API")
 public class AuthController {
 
     private final AuthService authService;
 
 
+    @Operation(summary = "회원가입", description = "회원가입을 진행합니다."
+            , tags = {"인증/인가"}, responses = {
+        @ApiResponse(responseCode = "201", description = "회원가입 성공"),
+        @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+        @ApiResponse(responseCode = "409", description = "이미 존재하는 이메일"),
+        @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     @PostMapping("/signin")
     public ResponseEntity<Void> signin(@Valid @RequestBody SigninRequestDto dto) {
         authService.signin(dto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @Operation(summary = "로그인", description = "로그인을 진행합니다."
+            , tags = {"인증/인가"},responses = {
+        @ApiResponse(responseCode = "200", description = "로그인 성공"),
+        @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+        @ApiResponse(responseCode = "401", description = "인증 실패"),
+        @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     @PostMapping("/login")
     public ResponseEntity<JwtAuthResponseDto> login(@Valid @RequestBody LoginRequestDto dto) {
-        return new ResponseEntity<>(authService.login(dto),HttpStatus.OK);
+        return new ResponseEntity<>(authService.login(dto), HttpStatus.OK);
     }
 
+    @Operation(summary = "로그아웃", description = "로그아웃을 진행합니다."
+            , tags = {"인증/인가"},responses = {
+        @ApiResponse(responseCode = "200", description = "로그아웃 성공"),
+        @ApiResponse(responseCode = "401", description = "로그인 필요"),
+        @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     @PostMapping("/logout")
     public ResponseEntity<Void> logout(HttpServletRequest request,
                                        HttpServletResponse response, Authentication authentication)

eb92836af896575a70fef22d267b20dce88bd166
이자드 <kimminkyeong0418@gmail.com>
2025-02-27 14:45:23 +0900

    feat : menu option entity 생성
    - menu option
    - menu option detail
    - 연관관계 설정
        - menu, menuOption, menuOptionDetail

---
+++ src/main/java/com/deliveryManPlus/entity/MenuOptionDetail.java
@@ -0,0 +1,32 @@
+package com.deliveryManPlus.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class MenuOptionDetail extends CreateDateEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private BigDecimal optionPrice;
+
+
+    @ManyToOne
+    @JoinColumn(name = "menu_option_id")
+    private MenuOption menuOption;
+
+    @Builder
+    public MenuOptionDetail(Long id, String name, BigDecimal optionPrice) {
+        this.id = id;
+        this.name = name;
+        this.optionPrice = optionPrice;
+    }
+}

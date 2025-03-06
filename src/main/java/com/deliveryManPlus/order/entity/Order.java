86a9766f118f6736982b7f9f594d6b5117d34089
이자드 <kimminkyeong0418@gmail.com>
2025-03-06 20:15:01 +0900

    feat : cart 생성에 의한 order entity 수정
    order 기능 자체는 거래가 완료되어 변함 없는 객체이기 때문에 menu의 기록을 남기는 책임은 menu 가 아닌 order 자체에 있다고 판단이 되었다. 그렇기 때문에 menu와 option의 기록을 남기는 객체를 menuHistory -> orderMenu, OrderMenuOptionDetail 로 변경

--- src/main/java/com/deliveryManPlus/order/entity/Order.java
+++ src/main/java/com/deliveryManPlus/order/entity/Order.java
@@ -1,17 +1,15 @@
 package com.deliveryManPlus.order.entity;
 
-import com.deliveryManPlus.common.exception.constant.errorcode.OrderStatus;
-import com.deliveryManPlus.common.entity.CreateAndUpdateDateEntity;
-import com.deliveryManPlus.shop.entity.Shop;
 import com.deliveryManPlus.auth.entity.User;
-import com.deliveryManPlus.menu.entity.MenuHistory;
+import com.deliveryManPlus.common.entity.CreateAndUpdateDateEntity;
+import com.deliveryManPlus.common.exception.constant.errorcode.OrderStatus;
+import com.deliveryManPlus.shop.entity.Shop;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -41,14 +39,16 @@ public class Order extends CreateAndUpdateDateEntity {
     @JoinColumn(name = "shop_id")
     private Shop shop;
 
-
     @Builder
-    public Order(MenuHistory menuHistory, User customer) {
+    public Order(BigDecimal totalPrice, User customer, List<OrderMenu> orderMenu, Shop shop) {
         this.status = OrderStatus.SUBMIT;
-        this.totalPrice = menuHistory.getPrice();
+        this.totalPrice = totalPrice;
+        this.rejectReason = rejectReason;
         this.customer = customer;
-        this.orderMenu = new ArrayList<>();
+        this.orderMenu = orderMenu;
+        this.shop = shop;
     }
+
     public void updateOrderMenu(List<OrderMenu> orderMenuList) {
         this.orderMenu.addAll(orderMenuList);
     }
--- src/main/java/com/deliveryManPlus/order/entity/OrderMenu.java
+++ src/main/java/com/deliveryManPlus/order/entity/OrderMenu.java
@@ -1,35 +1,41 @@
 package com.deliveryManPlus.order.entity;
 
-import com.deliveryManPlus.menu.entity.MenuHistory;
+import com.deliveryManPlus.common.entity.CreateDateEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Entity
 @NoArgsConstructor
 @Getter
-@Table(name = "orderd_menu")
-public class OrderMenu {
+public class OrderMenu extends CreateDateEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Long id;
 
-    private Integer quantity;
+    private String name;
+    private BigDecimal price;
 
-    @ManyToOne
-    @JoinColumn(name = "menuhistory_id")
-    private MenuHistory menuHistory;
+    private int quantity;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
+    @OneToMany(mappedBy = "orderMenu")
+    private List<OrderMenuOptionDetail> orderMenuOptionDetail;
+
     @Builder
-    public OrderMenu(MenuHistory menuHistory) {
-        this.menuHistory = menuHistory;
-        this.quantity = 1;
+    public OrderMenu(String menuName, BigDecimal menuPrice, int quantity) {
+        this.name = menuName;
+        this.price = menuPrice;
+        this.quantity = quantity;
     }
+
     public void updateOrder(Order order) {
         this.order = order;
     }

9cae7d100ff12a08c033ce56e9ef45457fdcc3b4
이자드 <kimminkyeong0418@gmail.com>
2025-03-12 15:19:32 +0900

    feat : 조회 기능 수량 추가

--- src/main/java/com/deliveryManPlus/coupon/dto/CouponResponseDto.java
+++ src/main/java/com/deliveryManPlus/coupon/dto/CouponResponseDto.java
@@ -12,6 +12,7 @@ public class CouponResponseDto {
     private final String name;
     private final String code;
     private final BigDecimal discountPrice;
+    private final Integer quantity;
     private final String startAt;
     private final String expiredAt;
 
@@ -22,6 +23,7 @@ public class CouponResponseDto {
         this.name = coupon.getName();
         this.code = coupon.getCode();
         this.discountPrice = coupon.getDiscountPrice();
+        this.quantity = coupon.getQuantity();
         this.startAt = coupon.getStartAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         this.expiredAt = coupon.getExpiredAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         this.couponBrandName = (coupon.getCouponBrandList() != null && !coupon.getCouponBrandList().isEmpty())
